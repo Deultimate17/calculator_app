3d9f1cd93c41b88e3c27d1291e11d10dc627520f
Deultimate17 <adigunibrahim17@gmail.com>
2024-02-07 23:48:52 +0100

    added feature

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'dart:math';
 import '../model/calc_model.dart';
 
 class CalculatorApp extends StatefulWidget {
@@ -14,12 +14,16 @@ class CalculatorApp extends StatefulWidget {
 class _CalculatorAppState extends State<CalculatorApp> {
    String display = '';
    List<String> assemble = [];
+   List<String> stack = [];
+   bool isAfter = false;
 
    int index = -1;
+   int index1 = -1;
    late double result;
    late double num1;
    late double num2;
    late String currentOperator = '';
+
 
 
   @override
@@ -30,8 +34,15 @@ class _CalculatorAppState extends State<CalculatorApp> {
         child: Column(
           children:[
             Container(
+              alignment: Alignment.topRight,
               height: 100,
-              child: Text(display),
+              child: Text(display,
+              style: const TextStyle(
+                fontSize: 23,
+
+              ),
+                textAlign: TextAlign.right,
+              ),
             ),
             Expanded(
               child: GridView.builder(
@@ -64,11 +75,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
             onTap: () {
          setState(() {
            if (calculate.numbers != 'C'
-               && calculate.numbers != '+'
-               && calculate.numbers != '/'
-               && calculate.numbers != 'x'
-               && calculate.numbers != '-'
-               && calculate.numbers != '%'
+           && !isOperator(calculate.numbers)
                && calculate.numbers != '='
                && calculate.numbers != '+/-'
            ) {
@@ -77,17 +84,34 @@ class _CalculatorAppState extends State<CalculatorApp> {
              assemble.add(calculate.numbers);
              display += assemble[index];
              print('you suck man');
-           } else if ( calculate.numbers == '+'
-           || calculate.numbers == '/'
-           || calculate.numbers == 'x'
-           || calculate.numbers == '-'
-           || calculate.numbers == '%') {
+           } else if (isOperator(calculate.numbers)) {
+             // index1++;
+             // stack.add(calculate.numbers);
+             // display += stack[index1];
+             // print(stack);
+             // currentOperator = calculate.numbers;
              index++;
+             print(index);
              assemble.add(calculate.numbers);
              display += assemble[index];
-             currentOperator = calculate.numbers;
            } else if (calculate.numbers == '=') {
-             print(assemble);
+             final expression = assemble.join('');
+             print(expression);
+             final result = evaluateExpression(expression);
+             if (result != null) {
+               assemble = [result.toString()];
+               print(assemble);
+               display = result.toString();
+               print(display);
+               index = 0;
+             } else if (calculate.numbers == 'C') {
+               display = '';
+             }
+             else {
+               display = 'Error';
+               assemble = [];
+               index = -1;
+             }
            }
            else {
              print('you rock');
@@ -97,4 +121,55 @@ class _CalculatorAppState extends State<CalculatorApp> {
       ),
     );
   }
+
+
+   num? evaluateExpression(String expression) {
+     try {
+       if (expression.contains('+') || expression.contains('-') || expression.contains('x') || expression.contains('/') || expression.contains('%')) {
+         final result = Function.apply(_performOperation, [expression]);
+         return result;
+       } else {
+         return num.parse(expression);
+       }
+     } catch (e) {
+       print('Error during expression evaluation: $e');
+       return null;
+     }
+   }
+
+   static num _performOperation(String expression) {
+     final parts = expression.split(RegExp(r'(\+|\-|x|\/|\%)'));
+     final operators = expression.split(RegExp(r'[0-9.]')).where((element) => element.isNotEmpty).toList();
+     num result = num.parse(parts.first);
+     for (int i = 0; i < operators.length; i++) {
+       final num nextNumber = num.parse(parts[i + 1]);
+       final op = operators[i];
+       switch (op) {
+         case '+':
+           result += nextNumber;
+           break;
+         case '-':
+           result -= nextNumber;
+           break;
+         case 'x':
+           result *= nextNumber;
+           break;
+         case '/':
+           result /= nextNumber;
+           break;
+         case '%':
+           result %= nextNumber;
+           break;
+       }
+     }
+     return result;
+   }
+   bool isOperator(String value) {
+    return value == '+' ||
+        value  == '-' ||
+        value == 'x' ||
+        value == '/' ||
+     value == '%';
+   }
+
 }
